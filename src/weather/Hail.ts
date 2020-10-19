d380ddc98f8a9c207aadc6fcd6c380cd802051e7
Evan Kysley <evan@kysley.com>
2020-10-19 00:27:39 -0400

    feat: first try at weather, Hail

---
+++ src/weather/Hail.ts
@@ -0,0 +1,24 @@
+import produce from 'immer';
+
+class Hail implements WeatherEffect {
+  name = 'Hail';
+
+  roll({ player }: { player: Player }) {
+    const roll = Math.random() * 100;
+    if (roll < 5) return true;
+    return false;
+  }
+
+  effect({ player }: { player: Player }) {
+    const event: EventMessage = {
+      info: {
+        name: 'Hail Storm',
+        happening: `Well Jim, ${player.name}'s ball was hit by stray hail!`,
+        triggeredBy: 'Hail',
+      },
+    };
+    return event;
+  }
+}
+
+export { Hail };
