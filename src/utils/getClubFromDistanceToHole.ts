6023fa2e20155d8dc7f290d309593aa449557c74
Evan Kysley <evan@kysley.com>
2020-10-19 00:39:08 -0400

    fix: choose driver if the distance is over 212

--- src/utils/getClubFromDistanceToHole.ts
+++ src/utils/getClubFromDistanceToHole.ts
@@ -9,7 +9,7 @@ export function getClubFromDistanceToHole(distanceToHole: number): Clubs {
   else if (distanceToHole > 183 && distanceToHole < 194) club = '5';
   else if (distanceToHole > 194 && distanceToHole < 203) club = '4';
   else if (distanceToHole > 203 && distanceToHole < 212) club = '3';
-  else if (distanceToHole > 212 && distanceToHole < 275) club = 'D';
+  else if (distanceToHole > 212) club = 'D';
   else {
     throw new Error(
       'Could not find a suitable club? distance: ' + distanceToHole
