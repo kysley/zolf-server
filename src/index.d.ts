5559307e8627abc48ffa9759f048f1ac3573a84c
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2020-10-24 09:38:00 -0400

    Revert "Revert "Revert "change: reworked interface and classes"""
    This reverts commit 023dc26269088d75f34456162731b09403334d3e.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,26 +1,13 @@
-// NOTE: We should think about how these types are being used. If there is any case
-// where we would be passing props to them ie. { prop: value } as Type then it would
-// be better to write them as interfaces.
-//
-// NOTE: Types are mostly used for functions or complex types ie. tuples
-// Complex type example:
-//
-// type MyComplexType = [string, number, Array<string>]
-//
-// Function example:
-//
-// type Person = {
-//   name: string,
-//   age: number
-// };
-//
-// type ReturnPerson = (
-//   person: Person
-// ) => Person;
-//
-// const returnPerson: ReturnPerson = (person) => {
-//   return person;
-// };
+type Lie = {
+  condition:
+    | 'Rough'
+    | 'Fescue'
+    | 'Heavy Rough'
+    | 'Fairway'
+    | 'Teebox'
+    | 'Green';
+  distanceToHole: number;
+};
 
 type Clubs = 'P' | 'AW' | 'PW' | '9' | '8' | '7' | '6' | '5' | '4' | '3' | 'D';
 
@@ -37,34 +24,19 @@ type Course = {
   holes: Hole[];
 };
 
-type Affliction = {};
-
-type WeatherNames = 'Hail';
-type AfflictionNames = 'Roid Rage'; // I lol'd at this
-
-type EventMessage = {
-  info: {
-    name: string;
-    happening: string;
-    triggeredBy: WeatherNames | AfflictionNames;
-  };
-  state?: Stats;
-};
-
-// Changed this to an interface to add extensibility
-interface Stats {
+type PlayerStats = {
   control: number;
   patience: number;
   brainSize: number;
   tilt: number;
   alive: boolean;
   proficiency: ClubProficiency;
-}
+};
 
-interface Lie {
-  condition: string;
-  distanceToHole: number;
-}
+type Affliction = {};
+
+type WeatherNames = 'Hail';
+type AfflictionNames = 'Roid Rage';
 
 interface Round {
   players: Player[];
@@ -76,17 +48,26 @@ interface Player {
   id: string;
   name: string;
   hole: number;
+  scoreCard: number[];
+  afflictions: Affliction[];
+  strokes: number;
+
+  stats: PlayerStats;
+  lie: Lie;
   course: Course;
-
-  scoreCard?: number[];
-  afflictions?: Affliction[];
-  strokes?: number;
-  stats?: Stats;
-  lie?: Lie;
-
   start(): void;
+
   swing(): void;
 }
+
+type EventMessage = {
+  info: {
+    name: string;
+    happening: string;
+    triggeredBy: WeatherNames | AfflictionNames;
+  };
+  state?: PlayerStats;
+};
 
 interface WeatherEffect {
   name: string;
